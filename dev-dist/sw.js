51b22c9b7e7cacb155ecb454570eaafcefa17bd5
Fakasy <Fatakm868@gmail.com>
2025-05-23 16:48:48 +0700

    menambahkan chart di dashboard (visual)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-894bd07a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0gocpnlijto"
+    "revision": "0.a5jleftm2qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
